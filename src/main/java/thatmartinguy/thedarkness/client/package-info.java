42da3659a9730df8558ab5ff5fadb4c280c65ad6
Choonster TheMage <Choonster.2010@gmail.com>
2017-02-26 04:39:38 +1100

    Add package-info.java files to every package with the same annotations as the ones auto-generated by MCP

---
+++ src/main/java/thatmartinguy/thedarkness/client/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package thatmartinguy.thedarkness.client;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/thatmartinguy/thedarkness/item/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package thatmartinguy.thedarkness.item;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
